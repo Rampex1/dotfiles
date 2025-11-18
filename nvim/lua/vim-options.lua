72f845f5dca32296393b3ff7ac74d4a4e4ce1002
David Zhou <67666861+Rampex1@users.noreply.github.com>
2025-11-18 13:07:02 +0800

    Standardize tmux nvim windown navigation

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -6,11 +6,5 @@ vim.g.mapleader = " "
 
 vim.opt.swapfile = false
 
--- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
