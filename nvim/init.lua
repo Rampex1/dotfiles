82b6ef0b7e801dd4496473cf2dee5ffe35eb4972
David Zhou <67666861+Rampex1@users.noreply.github.com>
2025-12-12 16:34:43 +0800

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("vim-options")
+
+-- Disable automatic comment continuation
+local group = vim.api.nvim_create_augroup("DisableAutoCommentContinuation", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  group = group,
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
